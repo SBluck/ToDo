d51cd56d5425b0adfc1813c9267e953b7dbea0c4
Sue Bluck <SBluck@academytrainee.com>
2020-12-13 22:15:56 +0000

    SB: update ToDO app and TdListController

--- src/main/java/com/example/demo/ToDoApplication.java
+++ src/main/java/com/example/demo/ToDoApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ToDoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToDoApplication.class, args);
+		ApplicationContext beanBag = SpringApplication.run(ToDoApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/example/demo/rest/TdListController.java
+++ src/main/java/com/example/demo/rest/TdListController.java
@@ -2,6 +2,7 @@ package com.example.demo.rest;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -25,6 +26,7 @@ public class TdListController {
 
 	private TdListService service;
 
+	@Autowired
 	public TdListController(TdListService service) {
 		super();
 		this.service = service;
