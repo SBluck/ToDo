f5c1aa6a777c34af7b44467aa016d22be6a6e96e
Sue Bluck <SBluck@academytrainee.com>
2020-12-17 13:36:03 +0000

    SB: add tdl-data.sql

---
+++ src/test/resources/tdl-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tdList (`name`) VALUES ('Work');
+INSERT INTO tdList (`name`) VALUES ('Home');
+INSERT INTO tdList (`name`) VALUES ('Shopping');
+INSERT INTO tdTask (todo) VALUES ('Attend daily stand-up');
+INSERT INTO tdTask (todo) VALUES ('Review Broadband contract');
+INSERT INTO tdTask (todo) VALUES ('Pasta (lots)')
